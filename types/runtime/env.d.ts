041dc0f7928ee5eb8fdd757f146de00c77cd55fa
no-turno <davispindola2828@gmail.com>
2024-06-08 08:56:28 -0300

    "Remove declaration for CSS module"

---
+++ types/runtime/env.d.ts
@@ -0,0 +1,9 @@
+declare module "bun" {
+    interface Env {
+        PORT: number | string
+        HOSTNAME: string
+        OUTDIR: string
+        SSR: boolean
+        NODE_ENV: "development" | "production"
+    }
+}
